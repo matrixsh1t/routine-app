74786c302a6627942dc4b0637e1b92377bd5833d
roman <roman.stupko@gmail.com>
2022-12-14 06:41:06 +0600

    initial commit

---
+++ src/main/kotlin/kz/webapp/routine/RoutineApplication.kt
@@ -0,0 +1,11 @@
+package kz.webapp.routine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoutineApplication
+
+fun main(args: Array<String>) {
+    runApplication<RoutineApplication>(*args)
+}
---
+++ src/test/kotlin/kz/webapp/routine/RoutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package kz.webapp.routine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoutineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
