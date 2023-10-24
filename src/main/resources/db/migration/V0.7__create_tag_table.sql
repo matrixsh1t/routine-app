9290c965b3df8c7ce124a954a6d03ebe49f5903a
roman <roman.stupko@gmail.com>
2023-10-25 03:39:05 +0600

    style: changed btn style in show-task

--- src/main/resources/db/migration/V0.7__create_tag_table.sql
+++ src/main/resources/db/migration/V0.7__create_tag_table.sql
@@ -2,3 +2,9 @@ CREATE TABLE IF NOT EXISTS tags (
    tag_id serial PRIMARY KEY NOT NULL,
    tag_name varchar(50)
 );
+
+CREATE TABLE IF NOT EXISTS task_tags (
+    task_id int8 not null references tasks,
+    tag_id int8 not null references tags,
+    PRIMARY KEY (task_id, tag_id)
+);
